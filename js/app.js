63322a53eb9b39436f698535e281ff05ae972d74
JankleyGS <32181601+JankleyGS@users.noreply.github.com>
2017-11-11 12:02:57 -0800

    attempted js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,21 @@
 $(document).foundation()
-// $(document).ready(function(){
-//     $('.carousel').carousel();
-//   });
+//////////////greeting script/////////////////
+//new date object//
+var today = new Date();
+//current hr//
+var hourNow = today.getHours();
+var greeting;
+//greeting according to time of day
+if(hourNow>18){
+greeting = 'Good Evening';
+}
+else if(hourNow>12){
+  greeting = 'Good Afternoon';
+}
+else if(hourNow>0){
+  greeting = 'Good Morning';
+}
+else {
+  greeting = 'Welcome';
+}
+document.write(greeting);
