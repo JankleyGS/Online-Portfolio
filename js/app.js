6c29ae26ea7004faf513c3d0cae0f7923229b9b4
JankleyGS <32181601+JankleyGS@users.noreply.github.com>
2017-12-06 22:52:02 -0800

    redesined
    better nav, more js, improved landing page, additional small changes to font and color

--- js/app.js
+++ js/app.js
@@ -6,36 +6,56 @@ var hourNow = today.getHours();
 var greeting = document.getElementById("greeting");
 var image = document.getElementById("image");
 var navigation = document.getElementById("navigation");
+
+
+
 //greeting according to time of day
 if(hourNow>18){
 greeting = '<h3>Good Evening!</h3>';
-image.src= "images/dark_sam_web.png";
+image.src = 'images/dark_sam_web.png';
+
+
 }
 else if(hourNow>12){
   greeting = '<h3>Good Afternoon!</h3>';
-  image.src= "images/buddha_blue.png";
+  image.src = 'images/buddha_blue.png';
 }
 else if(hourNow>0){
   greeting = '<h3>Good Morning!</h3>';
-  image.src= "images/eggato.png";
+  image.src = 'images/eggato.png';
 }
 else {
   greeting = '<h3>Welcome!</h3>';
 }
 
+// img change depending on time
+if(hourNow>18){
+image.src = 'images/dark_sam_web.png';
+}
+else if(hourNow>12){
+  image.src = 'images/buddha_blue.png';
+}
+else if(hourNow>0){
+  image.src = 'images/eggato.png';
+}
+else {
+  image.src = 'images/dwayne_bird_web.png';
+}
+
 document.write(greeting);
+
+
+
 window.onscroll = function(){
 
   var y = window.pageYOffset;
-      var x = window.pageXOffset;
+  var x = window.pageXOffset;
       console.log(y);
       console.log(x);
 
-
   if( y > 1){
-        navigation.style.top = y + "px";
-
+        navigation.style.backgroundColor = "#000000";
         } else {
-        navigation.style.top = "0px" ;
+        navigation.style.backgroundColor = "#ffffff";
         }
 }
