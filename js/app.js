b64517db7475af6c8a4394f1e3c2700998d2be4a
JankleyGS <32181601+JankleyGS@users.noreply.github.com>
2017-12-07 14:26:26 -0800

    fixed greeting

--- js/app.js
+++ js/app.js
@@ -7,55 +7,41 @@ var greeting = document.getElementById("greeting");
 var image = document.getElementById("image");
 var navigation = document.getElementById("navigation");
 
+console.log(hourNow);
 
-
-//greeting according to time of day
+//greeting && image according to time of day
 if(hourNow>18){
-greeting = '<h3>Good Evening!</h3>';
-image.src = 'images/dark_sam_web.png';
-
-
-}
-else if(hourNow>12){
-  greeting = '<h3>Good Afternoon!</h3>';
-  image.src = 'images/buddha_blue.png';
-}
-else if(hourNow>0){
-  greeting = '<h3>Good Morning!</h3>';
-  image.src = 'images/eggato.png';
-}
-else {
-  greeting = '<h3>Welcome!</h3>';
-}
-
-// img change depending on time
-if(hourNow>18){
+greeting.innerHTML = 'Good Evening!';
+greeting.style.color = '#D40056';
 image.src = 'images/dark_sam_web.png';
 }
 else if(hourNow>12){
+  greeting.innerHTML = 'Good Afternoon!';
+  greeting.style.color = '#093DA4';
   image.src = 'images/buddha_blue.png';
 }
 else if(hourNow>0){
+  greeting.innerHTML = 'Good Morning!';
+  greeting.style.color = '#F6A100';
   image.src = 'images/eggato.png';
 }
 else {
+  greeting.innerHTML = 'Welcome!';
+  greeting.style.color = '#494847';
   image.src = 'images/dwayne_bird_web.png';
 }
 
-document.write(greeting);
 
-
-
-window.onscroll = function(){
-
-  var y = window.pageYOffset;
-  var x = window.pageXOffset;
-      console.log(y);
-      console.log(x);
-
-  if( y > 1){
-        navigation.style.backgroundColor = "#000000";
-        } else {
-        navigation.style.backgroundColor = "#ffffff";
-        }
-}
+// window.onscroll = function(){
+//
+//   var y = window.pageYOffset;
+//   var x = window.pageXOffset;
+//       console.log(y);
+//       console.log(x);
+//
+//   if( y > 1){
+//         navigation.style.backgroundColor = "#000000";
+//         } else {
+//         navigation.style.backgroundColor = "#ffffff";
+//         }
+// }
