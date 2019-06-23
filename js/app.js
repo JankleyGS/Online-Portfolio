855cd05ebc64fb1a146a2b225efb59f57dcd50af
JankleyGS <32181601+JankleyGS@users.noreply.github.com>
2019-06-22 21:52:52 -0700

    played with carousels

--- js/app.js
+++ js/app.js
@@ -7,13 +7,28 @@ var greeting = document.getElementById("greeting");
 var image = document.getElementById("image");
 var navigation = document.getElementById("navigation");
 
-console.log(hourNow);
+///////////carousels////
+// var slideIndex = 0;
+// carousel();
+
+
+///////dot slide
+var slideIndex = 1;
+var i;
+var x = document.getElementsByClassName("mySlides");
+var dots = document.getElementsByClassName("demo");
+if (n > x.length) {slideIndex = 1}
+if (n < 1) {slideIndex = x.length}
+
+
 
 //greeting && image according to time of day
+console.log(hourNow);
+
 if(hourNow>18){
 greeting.innerHTML = 'Good Evening!';
 greeting.style.color = '#ffffff';
-image.src = 'images/dark_sam_web.png';
+image.src = 'images/dogAndGod.png';
 }
 else if(hourNow>12){
   greeting.innerHTML = 'Good Afternoon!';
@@ -38,6 +53,50 @@ function myFunction() {
     } else {
         x.className = "topnav";
     }
+}
+
+//////////////carousel function
+
+// function carousel() {
+//   var i;
+//   var x = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > x.length) {slideIndex = 1}
+//   x[slideIndex-1].style.display = "block";
+//   setTimeout(carousel, 2000); // Change image every 2 seconds
+// }
+
+//////////////////carousel dot slideIndex
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" white", "");
+  }
+  x[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " white";
 }
 // window.onscroll = function(){
 //
